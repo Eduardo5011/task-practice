6d248489e2de2f2d63d0f07086295ee6873a7e73
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-05-23 20:50:27 -0400

    updated

--- src/Items.jsx
+++ src/Items.jsx
@@ -3,16 +3,32 @@ import SingleItem from "./SingleItem";
 import customFetch from "./utils";
 useQuery;
 
-const Items = ({ items }) => {
-  const result = useQuery({
-    queryKey: ['tasks'],
-    queryFn: () => customFetch.get("/"),
+const Items = () => {
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get("/something");
+      return data;
+    },
   });
-  console.log(result);
+
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
+
+  if(isError){
+    return <p style={{ marginTop: "1rem" }}>There was an error...</p>
+  }
+
+  //reference
+// console.log(error)
+//   if(error){
+//     return <p style={{ marginTop: "1rem" }}>{error.response.data}</p>
+//   }
 
   return (
     <div className="items">
-      {items.map((item) => {
+      {data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
