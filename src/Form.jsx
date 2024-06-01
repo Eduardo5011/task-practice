306bbd625da3d5d42f25c546edc525b3ed53b296
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-06-01 18:21:33 -0400

    updated

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,23 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import customFetch from "./utils";
+import { toast } from "react-toastify";
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState("");
+  const QueryClient = useQueryClient();
 
-  const {mutate: createTask, isLoading} = useMutation({
+  const { mutate: createTask, isLoading } = useMutation({
     mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+    onSuccess: () => {
+      QueryClient.invalidateQueries({ queryKey: ["tasks"] });
+      toast.success("task added");
+      setNewItemName("");
+    },
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
   });
- 
 
   const handleSubmit = (e) => {
     e.preventDefault();
